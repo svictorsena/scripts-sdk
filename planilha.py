3e531079e2f1c35077c0a90d98f9588be53e3e68
samuel <81726687+svictorsena@users.noreply.github.com>
2024-11-05 17:55:06 -0300

    Update planilha.py

--- planilha.py
+++ planilha.py
@@ -15,14 +15,14 @@ t = CTk()
 t.geometry("800x500")
 t.resizable(False, False)
 
-entrada = CTkLabel(t, width=200, text="Selecione a planilha", font=("Arial", 20, "bold"))
-entrada.pack(pady=20)
+texto = CTkLabel(t, width=200, text="Selecione a planilha", font=("Arial", 20, "bold"))
+texto.pack(pady=20)
 
 f = [i for i in os.listdir() if i.endswith(".xlsx")]
 
-def io():
-	global cpf_column, nome_column, sheet, button
-	entrada.configure(text="bagulho aqui")
+def adicionar():
+	global cpf_column, nome_column, sheet, add_button
+	texto.configure(text="bagulho aqui")
 	s = sheet.cell(1, cpf_column + 1).value
 	if "str" in str(type(s)):
 		sheet.delete_rows(1)
@@ -30,56 +30,77 @@ def io():
 	time.sleep(10)
 	for i in sheet.values:
 		pyautogui.write(str(i[cpf_column]))
-		time.sleep(1)
 		pyautogui.press("tab")
-		time.sleep(1)
 		pyautogui.write(str(i[nome_column]))
-		time.sleep(1)
 		pyautogui.press("tab")
 		pyautogui.press("tab")
 		if keyboard.is_pressed("esc"):
 			break
-
-def oii(event):
-	global cpf_column, nome_column, button, cb2
-	nome_column = column_index_from_string(event) - 1
+	try:
+		for i in range(0, 3):
+		    pyautogui.press("tab")
+	finally:
+		pyautogui.press("enter")
 	
-	cb2.pack_forget()
-	entrada.configure(text="Tudo certinho! Só apertar o botão abaixo para iniciar o processo.")
+def deletar():
+	global cpf_column, nome_column, sheet, del_button
+	texto.configure(text="bagulho aqui")
+	cell = sheet.cell(1, cpf_column + 1).value
+	if "str" in str(type(cell)):
+		sheet.delete_rows(1)
 	
-	button = CTkButton(t, text="iniciar", width=100, command=io).pack()
+	time.sleep(10)
+	for valor in sheet.values:
+		pyautogui.write(str(valor[cpf_column]))
+		pyautogui.press("tab")
+		pyautogui.write(str(valor[nome_column]))
+		pyautogui.press("tab")
+		pyautogui.press("tab")
+		if keyboard.is_pressed("esc"):
+			break
+	try:
+		for i in range(0, 4):
+		    pyautogui.press("tab")
+	finally:
+		pyautogui.press("enter")
+	 
+def get_cpf_column(column):
+	global cpf_column, nome_column, add_button, del_button
+	cpf_column = column_index_from_string(column) - 1
 
-def oi(column):
-    global cb1, k, cpf_column, cb2
-    cpf_column = column_index_from_string(column) - 1
-    entrada.configure(text="Selecione a coluna em que está o nome")
-    cb1.pack_forget()
-    
-    k = [x for x in k if x != column]
+	cb.destroy()
+
+	texto.configure(text="Tudo certinho! Só apertar o botão abaixo para iniciar o processo.")
 	
-    cb2 = CTkComboBox(t, width=300, values=k, command=oii)
-    cb2.set("--selecione--")
-    cb2.pack()
+	add_button = CTkButton(t, text="Adicionar", width=100, command=adicionar).pack(side="left", padx=(290, 10), pady=(0, 400))
+	del_button = CTkButton(t, text="Deletar", width=100, command=deletar).pack(side="left", padx=(10, 290), pady=(0, 400))
 
-def value(event):
-    global k, cb1, sheet# Tornando k uma variável global
+def get_nome_column(column):
+	global colunas, nome_column
+	nome_column = column_index_from_string(column) - 1
+	texto.configure(text="Selecione a coluna em que está o CPF")
+	
+	colunas = [x for x in colunas if x != column]
+	
+	cb.configure(values=colunas, command=get_cpf_column)
+	cb.set("--selecione--")
+
+def planilha(_):
+    global colunas, sheet
     planilha = cb.get()
     wb = load_workbook(planilha)
     sheet = wb.active
     
     max_column = sheet.max_column
     
-    k = [get_column_letter(i) for i in range(1, max_column + 1)]
+    colunas = [get_column_letter(i) for i in range(1, max_column + 1)]
     
-    entrada.configure(text="Selecione a coluna em que está o CPF")
-    cb.pack_forget()
+    texto.configure(text="Selecione a coluna em que está o nome")
 
-    cb1 = CTkComboBox(t, width=300, values=k, command=oi)
-    cb1.set("--selecione--")
-    cb1.pack()
-
-cb = CTkComboBox(t, width=300, values=f, command=value)
-cb.set("--selecione--")
+    cb.configure(values=colunas, command=get_nome_column)
+    cb.set("--selecione--")
+    
+cb = CTkComboBox(t, width=300, values=f, command=planilha)
 cb.pack()
-
+cb.set("--selecione--")
 t.mainloop()
